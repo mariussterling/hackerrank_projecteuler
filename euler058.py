b53e0ff78a7378b46dba8891def7fc2f5a1f9795
mariussterling <marius.sterling@hu-berlin.de>
2020-02-24 08:05:06 +0100

    added euler058.py

---
+++ euler058.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 17:12:33 2020
+
+@author: ms
+"""
+ # Enter your code here. Read input from STDIN. Print output to STDOUT
+from math import sqrt
+N = int(input())
+def _try_composite(a, d, n, s):
+    if pow(a, d, n) == 1:
+        return False
+    for i in range(s):
+        if pow(a, 2**i * d, n) == n-1:
+            return False
+    return True # n  is definitely composite
+ 
+def is_prime(n, _precision_for_huge_n=16):
+    if n in _known_primes:
+        return True
+    if any((n % p) == 0 for p in _known_primes) or n in (0, 1):
+        return False
+    d, s = n - 1, 0
+    while not d % 2:
+        d, s = d >> 1, s + 1
+    # Returns exact according to http://primes.utm.edu/prove/prove2_3.html
+    if n < 1373653: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3))
+    if n < 25326001: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5))
+    if n < 118670087467: 
+        if n == 3215031751: 
+            return False
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7))
+    if n < 2152302898747: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11))
+    if n < 3474749660383: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13))
+    if n < 341550071728321: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17))
+    # otherwise
+    return not any(_try_composite(a, d, n, s) 
+                   for a in _known_primes[:_precision_for_huge_n])
+
+_known_primes = [2, 3]
+_known_primes += [x for x in range(5, 1000, 2) if is_prime(x)]
+
+diag_primes = 0
+pratio = 1
+i = 1
+N /= 100
+
+while True:
+    off = (2*i+1)**2
+    diag_primes += sum([is_prime(off - 2 * j * i) for j in range(1, 4)])
+    if diag_primes < N * (4 * i + 1):
+        break
+    i += 1
+print(2 * i + 1)
